dfff7fef66e625437254f0d8cfcf645f16e0c3ca
chavierto <hola@xavierlujan.com>
2021-03-25 23:41:57 -0400

    refactor day 11

--- 30daysofcode/11.js
+++ 30daysofcode/11.js
@@ -1,7 +1,7 @@
 function main() {
 	let arr = Array(6);
 	let sums = [];
-	let k = 0;
+	
 
 	for (let i = 0; i < 6; i++) {
 		arr[i] = readLine()
@@ -12,17 +12,16 @@ function main() {
 	// Define how to go over hourglass patterns to add items to array.
 	for (let i = 0; i < 4; i++) {
 		for (let j = 0; j < 4; j++) {
-			// Each hourglass pattern will be pushed to an array.
-			// Reduce arrays to their sum.
-			sums[k] =
-				arr[i][j] +
+			// Each hourglass sum will be pushed to an array.
+            
+			sums.push(arr[i][j] +
 				arr[i][j + 1] +
 				arr[i][j + 2] +
 				arr[i + 1][j + 1] +
 				arr[i + 2][j] +
 				arr[i + 2][j + 1] +
-				arr[i + 2][j + 2];
-			k++;
+				arr[i + 2][j + 2]);
+			
 		}
 	}
 	// Highest sum in array is printed
