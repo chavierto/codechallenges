e070f32dbf1501cfc0bb7b586b233c2692da2c5f
chavierto <hola@xavierlujan.com>
2021-04-05 12:15:22 -0400

    Learn linked lists, add remove method

--- linked_lists.js
+++ linked_lists.js
@@ -26,6 +26,53 @@ class LinkedList {
 		}
 		this._length++;
 	}
+
+	remove(value) {
+		// Check if value exists in list
+		if (this.contains(value)) {
+			// begin at start of the list (head)
+			let current = this._head;
+			let previous = this._head;
+			while (current) {
+				if (current.value === value) {
+					if (this._head === current) {
+						// if it's the head
+						this._head = this._head.next;
+						// reset the head
+						this._length--;
+						// update length
+						return;
+					}
+
+					if (this._tail === current) {
+						// if it's the tail
+						this._tail = previous;
+						// reset tail
+					}
+
+					previous.next = current.next;
+					// unlink
+					this._length--;
+					// update length
+					return;
+				}
+				previous = current;
+				// look at the next node
+				current = current.next;
+			}
+		}
+	}
+
+	contains(value) {
+		let node = this._head;
+		while (node) {
+			if (node.value === value) {
+				return true;
+			}
+			node = node.next;
+		}
+		return false;
+	}
 }
 
 class Node {
@@ -39,4 +86,7 @@ const AmazingRace = new LinkedList();
 AmazingRace.add('Caracas,Venezuela');
 console.log(AmazingRace);
 AmazingRace.add('Miami,FL');
+console.log(AmazingRace);
+console.log(AmazingRace.contains('Miami,FL'));
+AmazingRace.remove('Caracas,Venezuela');
 console.log(AmazingRace);
